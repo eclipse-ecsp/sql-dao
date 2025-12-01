0c58fe571816df587cce89284a1bf5c462884849
Rajendran <Rajendran.P@harman.com>
2025-12-01 09:24:02 +0000

    Updated to load default tenant from system property

--- src/main/java/org/eclipse/ecsp/sql/dao/constants/MultitenantConstants.java
+++ src/main/java/org/eclipse/ecsp/sql/dao/constants/MultitenantConstants.java
@@ -54,9 +54,12 @@ public class MultitenantConstants {
     /** Multitenancy related properties. */
     public static final String MULTITENANCY_ENABLED = "multitenancy.enabled";
 
+    /** The Constant TENANT_DEFAULT for system property. */
+    public static final String TENANT_DEFAULT = "tenant.default";
+
     /** The Constant DEFAULT_TENANT_ID. */
     public static final String DEFAULT_TENANT_ID = "default";
-
+    
     /** The Constant MULTI_TENANT_IDS. */
     public static final String MULTI_TENANT_IDS = "multi.tenant.ids";
 }
